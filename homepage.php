4478441ab04e78f6ac26ee98ffc7d8ee3d853d13
Tyree <tyreerussellmccloud@gmail.com>
2024-11-06 18:53:39 -0500

    added directions and hangman img

--- homepage.php
+++ homepage.php
@@ -15,7 +15,16 @@ session_destroy();
     <div class="header">Hangman Game</div>
 
     <div class="main">
-        <h1>Select Difficulty</h1>
+    <h1>Welcome to Hangman! Here's how to play</h1>
+    <ul>
+        <li>Choose a difficulty level to begin the game.</li>
+        <li>A word will be chosen, and you'll have to guess each letter.</li>
+        <li>Guess one letter at a time to reveal parts of the word.</li>
+        <li>For every incorrect guess, a part of the hangman is drawn.</li>
+        <li>The game ends when you guess the word correctly or the hangman is fully drawn.</li>
+        <li>Good luck, and have fun!</li>
+    </ul>
+     <img src="img/hangman.png" alt="hangman img" class="hangman-image">
     </div>
 
     <div class="bottom">
